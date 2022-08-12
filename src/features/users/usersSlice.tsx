7c361c7b0328be27e7dde2fbd1d6b264255f3530
SteveReid <steve@appby.design>
2022-08-12 18:31:42 +0100

    create a selectUserById selector and add a users list component

--- src/features/users/usersSlice.tsx
+++ src/features/users/usersSlice.tsx
@@ -30,4 +30,7 @@ const usersSlice = createSlice({
 
 export const selectAllUsers = (state: RootState) => state.users;
 
+export const selectuserById = (state: RootState, userId: number) =>
+  state.users.find(user => user.id === userId);
+
 export default usersSlice.reducer;
